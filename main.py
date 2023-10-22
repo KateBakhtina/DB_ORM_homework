703713ef769a128596f75808c32f5ec8a7d1eae7
Kate Bakhtina <almazniegori@gmail.com>
2023-10-22 13:21:24 +0300

    add_func

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# import models
-#
-#
-# engine = create_engine(f"{input('Драйвер подключения: ')}://"
-#                            f"{input('Пользователь: ')}:"
-#                            f"{input('Пароль: ')}@"
-#                            f"localhost:{input('порт: ')}/"
-#                            f"{input('База данных: ')}")
-#
-# models.create_table(engine)
-#
-# Session = sessionmaker(bind=engine)
-# session = Session()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import Publisher, Book, Shop, Stock, Sale, create_table
+from json import load
+import os
+from dotenv import load_dotenv
+
+def write_data(data_name):
+    with open(data_name, encoding='utf-8') as file:
+        return load(file)
+
+def write_table(data):
+    models_dictionary = {
+        Publisher.__tablename__: Publisher,
+        Book.__tablename__: Book,
+        Shop.__tablename__: Shop,
+        Stock.__tablename__: Stock,
+        Sale.__tablename__: Sale
+    }
+    with Session(engine) as session:
+        for row in data:
+            model = models_dictionary.get(row.get('model'))
+            if model:
+                session.add(model(**row.get('fields')))
+                session.commit()
+
+def make_query(author):
+    with Session(engine) as session:
+        if author.isdigit():
+            q = session.query(Publisher, Book.title, Shop.name, Sale.price, Sale.date_sale) \
+                .join(Book) \
+                .join(Stock).join(Shop).join(Sale) \
+                .filter((Publisher.id == author)).all()
+            return q
+        else:
+            q = session.query(Publisher, Book.title, Shop.name, Sale.price, Sale.date_sale) \
+                .join(Book) \
+                .join(Stock).join(Shop).join(Sale) \
+                .filter((Publisher.name == author)).all()
+            return q
+
+def print_data(data):
+    size_title_book, size_name_shop = [len(x[1]) for x in data], [len(x[2]) for x in data]
+    for publisher, title, shop_name, price, date_sale in data:
+        print(f"{title.ljust(max(size_title_book))} | {shop_name.ljust(max(size_name_shop))} | {price} | {date_sale}")
+
+def get_book():
+    author = input('Введите id издателя или название:')
+    q = make_query(author)
+    if q:
+        print_data(q)
+    else:
+        print('Данные не найдены')
 
 
 
-from dotenv import dotenv_values
-config = dotenv_values()        
-print(config.get('NAME_DRIVER'))
+if __name__ == '__main__':
+    load_dotenv()
+    engine = create_engine(f"{os.getenv('NAME_DRIVER')}://"
+                           f"{os.getenv('USER')}:"
+                           f"{os.getenv('PASSWORD')}@"
+                           f"localhost:{os.getenv('LOCAL_HOST')}/"
+                           f"{os.getenv('DB')}")
+    create_table(engine)
 
-
-
+    write_table(write_data('tests_data.json'))
+    get_book()
 
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Numeric
 from sqlalchemy.orm import declarative_base, relationship
 from datetime import datetime
+
 
 
 Base = declarative_base()
@@ -9,64 +10,69 @@ Base = declarative_base()
 class Publisher(Base):
     __tablename__ = "publisher"
 
-    publisher_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(length=50), unique=True)
 
     def __str__(self):
-        return f"{self.publisher_id}: {self.name}"
+        return f"{self.id}: {self.name}"
+    def __repr__(self):
+        return f"{self.id}: {self.name}"
 
 
 class Book(Base):
     __tablename__ = "book"
 
-    book_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String(length=50), nullable=False)
-    publisher_id = Column(Integer, ForeignKey("publisher.publisher_id"), nullable=False)
+    id_publisher = Column(Integer, ForeignKey("publisher.id"), nullable=False)
 
     publisher = relationship(Publisher, backref="book")
 
     def __str__(self):
-        return f"{self.book_id}: ({self.title}, {self.publisher_id})"
+        return f"{self.id}: ({self.title}, {self.id_publisher})"
 
 
 class Shop(Base):
     __tablename__ = "shop"
 
-    shop_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(length=30), unique=True)
 
     def __str__(self):
-        return f"{self.shop_id}: {self.name}"
+        return f"{self.id}: {self.name}"
+
 
 
 class Stock(Base):
     __tablename__ = "stock"
 
-    stock_id = Column(Integer, primary_key=True)
-    book_id = Column(Integer, ForeignKey("book.book_id"), nullable=False)
-    shop_id = Column(Integer, ForeignKey("shop.shop_id"), nullable=False)
-    amount = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True)
+    id_book = Column(Integer, ForeignKey("book.id"), nullable=False)
+    id_shop = Column(Integer, ForeignKey("shop.id"), nullable=False)
+    count = Column(Integer, nullable=False)
 
     book = relationship(Book, backref="stock")
     shop = relationship(Shop, backref="stock")
 
+
     def __str__(self):
-        return f"{self.stock_id}: ({self.book_id}, {self.shop_id}, {self.amount})"
+        return f"{self.id}: ({self.id_book}, {self.id_shop}, {self.count})"
 
 
 class Sale(Base):
     __tablename__ = "sale"
 
-    sale_id = Column(Integer, primary_key=True)
-    price = Column(Float(2), nullable=False)
+    id = Column(Integer, primary_key=True)
+    price = Column(Numeric(10, 2), nullable=False)
     date_sale = Column(DateTime(timezone=True), default=datetime.utcnow())
-    stock_id = Column(Integer, ForeignKey("stock.stock_id"), nullable=False)
-    amount = Column(Integer, nullable=False)
-
-    def __str__(self):
-        return f"{self.sale_id}: ({self.price}, {self.date_sale}, {self.stock_id}, {self.amount})"
+    count = Column(Integer, nullable=False)
+    id_stock = Column(Integer, ForeignKey("stock.id"), nullable=False)
 
     stock = relationship(Stock, backref="sale")
+    def __str__(self):
+        return f"{self.id}: ({self.price}, {self.date_sale}, {self.id_stock}, {self.count})"
+
+
 
 
 def create_table(engine):
